c73de415f8ad5bfc95c5b037ca57ad6ff3c28a79
SunburtReynolds <jon.reynolds456@utexas.edu>
2014-10-08 19:00:52 -0500

    does not crash, no functionality

--- app/src/main/java/geocoding/reynolds/ee461l/utexas/edu/geocoding/MapsActivity.java
+++ app/src/main/java/geocoding/reynolds/ee461l/utexas/edu/geocoding/MapsActivity.java
@@ -2,72 +2,88 @@ package geocoding.reynolds.ee461l.utexas.edu.geocoding;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
+    com.google.android.gms.maps.MapView mapView;
     String APIkey = "AIzaSyCEjmzkWPOxEj8r_kDM2sX6w9qmY50WlSw";
+    EditText addressForm;
+    GoogleMap map;
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    // private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        // setUpMapIfNeeded();
+
+        addressForm = (EditText) findViewById(R.id.editText_addressForm);
+        mapView = (MapView) findViewById(R.id.mapView_map);
+
+        // Why?
+        mapView.onCreate(savedInstanceState);
+
+        map = mapView.getMap();
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Map settings
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.getUiSettings().setCompassEnabled(false);
+        map.getUiSettings().setZoomControlsEnabled(false);
+
+        try {
+            MapsInitializer.initialize(this);
+        }
+        catch (Exception e) {
+            Log.d("GOOGLE PLAY SERVICES", "NOT AVAILABLE");
+        }
+
+        mapView.onResume();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
+//    private void setUpMapIfNeeded() {
+//        // Do a null check to confirm that we have not already instantiated the map.
+//        if (map == null) {
+//            // Try to obtain the map from the SupportMapFragment.
+//            map = (MapView) findViewById(R.id.mapView_map);
+//            // Check if we were successful in obtaining the map.
+//            if (map != null) {
+//                setUpMap();
+//            }
+//        }
+//    }
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
+
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
     // TODO: test and add functionality
     public void searchAddress() {
-        Toast.makeText(this, "search address", Toast.LENGTH_SHORT);
+
+        String addressString = addressForm.getText().toString();
+        addressString.replace(" ", "+");
+
+        Toast.makeText(this, "addressString", Toast.LENGTH_SHORT).show();
     }
 }
